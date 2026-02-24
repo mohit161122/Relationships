a200f842bc9d9dfd256791033d6aeba0dfff51f8
Mohit <mohitbaghel1234321@gmail.com>
2026-02-24 18:16:31 +0530

    one to Squillions complete

--- Models/posts.js
+++ Models/posts.js
@@ -25,27 +25,37 @@ const postSchema = new Schema ({
     }
 });
 
-// const User = mongoose.model("User" , userSchema);
-// const Post = mongoose.model("Post" , postSchema);
+const User = mongoose.model("User" , userSchema);
+ const Post = mongoose.model("Post" , postSchema);
 
-// const addData = async() => {
-//     let user = await User.findOne({username: "rahul kumar"});
+//  const addData = async () => {
+//     let user = await User.findOne({ username: "rahulKumar"});
 
 //     let post2 = new Post({
-//         content: "bye bye:)",
-//         likes: 23,
+//         content: "Bye bye ",
+//         likes:23,
 //     });
-
 //     post2.user = user;
 //     await post2.save();
-//     // await post1.save();
+//  };
+
+//  addData();
+
+
+
+const getData = async () => {
+    let result = await Post.findOne({}).populate("user" , "username");
+    console.log(result);
+};
+
+getData(); 
+
+
+
+// const del  = async() => {
+//      await Post.findByIdAndDelete("699956fc935edc9e07339cc8");
+//      await User.findByIdAndDelete("69994ed4f502daf1205c706f");
+    
 // };
 
-// addData();
-
- const del = async() => {
-   await Post.findByIdAndDelete("6999561f94e829a76c13420d");
-//     await User.findByIdAndDelete("699883b6c70975c846dcdd94");
- };
-
-del();
+// del();
